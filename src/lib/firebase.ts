2fcb4af6aed08ae8f12f67eb14818e5e913454ee
Irvin Bito-on <migz89star@gmail.com>
2025-09-14 09:57:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// src/lib/firebase.ts
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  "projectId": "studio-7818144568-e6f2b",
+  "appId": "1:575946598737:web:2cf21d158139cea2b20b70",
+  "storageBucket": "studio-7818144568-e6f2b.appspot.com",
+  "apiKey": "AIzaSyDR8_qe_k6lSr1-_dbDFar_oMdU4kANRpQ",
+  "authDomain": "studio-7818144568-e6f2b.firebaseapp.com",
+  "messagingSenderId": "575946598737"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
