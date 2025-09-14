64068b4a5c3260be25b318093bf0f830655f9a04
Irvin Bito-on <migz89star@gmail.com>
2025-09-14 10:01:56 +0000

    Security headers & App Check: // next.config.ts (CSP + headers; see prio

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 import type {NextConfig} from 'next';
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com;
+  child-src 'self';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  img-src 'self' data: https://picsum.photos https://images.unsplash.com https://placehold.co;
+  connect-src 'self' https://*.firebaseio.com wss://*.firebaseio.com https://firestore.googleapis.com https://www.googleapis.com;
+  frame-src 'self' https://*.firebaseapp.com;
+`.replace(/\s{2,}/g, ' ').trim();
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -29,6 +41,35 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy,
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+           {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+           {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
   },
 };
 
