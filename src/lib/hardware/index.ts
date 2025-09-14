171c32cd97a8734794cc28078e18e5182793841a
Irvin Bito-on <migz89star@gmail.com>
2025-09-14 09:51:14 +0000

    // src/lib/hardware/index.ts import { nativeHardware } from './native' i

--- src/lib/hardware/index.ts
+++ src/lib/hardware/index.ts
@@ -1,11 +1,4 @@
 // src/lib/hardware/index.ts
 import { nativeHardware } from './native'
 import { webHardware } from './web'
-
-const isCapacitor = () => {
-  if (typeof window !== 'undefined' && (window as any).Capacitor?.isNativePlatform()) {
-    return true;
-  }
-  return false;
-}
-export const hardware = isCapacitor() ? nativeHardware : webHardware
+export const hardware = (globalThis as any).Capacitor ? nativeHardware : webHardware
